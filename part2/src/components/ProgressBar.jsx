cf49b44f56741066ebae591f6cde2e763f1f3ed1
sugamshrestha677 <xresthasugam@gmail.com>
2025-09-19 06:59:05 +0545

    ProgressBar.jsx modified

---
+++ part2/src/components/ProgressBar.jsx
@@ -0,0 +1,13 @@
+// components/ProgressBar.jsx
+const ProgressBar = ({ progress }) => {
+  return (
+    <div className="w-full bg-gray-200 rounded-full h-2.5">
+      <div 
+        className="bg-indigo-600 h-2.5 rounded-full" 
+        style={{ width: `${progress}%` }}
+      ></div>
+    </div>
+  )
+}
+
+export default ProgressBar
