f1aff36690962b6cf6c676d4eaabbcb1b8546d5d
sugamshrestha677 <xresthasugam@gmail.com>
2025-09-21 22:26:09 +0545

    LoadingSpinner.jsx modified

---
+++ part3/src/components/LoadingSpinner.jsx
@@ -0,0 +1,10 @@
+// components/LoadingSpinner.jsx
+const LoadingSpinner = () => {
+  return (
+    <div className="flex justify-center items-center h-64">
+      <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-indigo-600"></div>
+    </div>
+  )
+}
+
+export default LoadingSpinner
